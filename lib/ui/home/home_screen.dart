d73eec01e1751504f52751cb0ac8fa7748c32f65
Lucas David <lucasdavidoliveira80@gmail.com>
2025-05-15 23:07:36 -0300

    refactor(home_screen): deixando mais amigável

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:techtaste/data/categories_data.dart';
 import 'package:techtaste/data/restaurant_data.dart';
 import 'package:techtaste/model/restaurant.dart';
 import 'package:techtaste/ui/_core/widgets/app_bar.dart';
+import 'package:techtaste/ui/_core/widgets/drawer.dart';
 import 'package:techtaste/ui/home/widgets/category_widget.dart';
 import 'package:techtaste/ui/home/widgets/restaurant_widget.dart';
 
@@ -13,47 +14,60 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     RestaurantData restaurantData = Provider.of<RestaurantData>(context);
-    return Scaffold(
-      drawer: Drawer(),
-      appBar: getAppBar(context: context),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 32,
-            children: [
-              Center(child: Image.asset('assets/logo.png', width: 147)),
-              Text('Boas vindas!'),
-              TextFormField(),
-              Text('Escolha por categoria'),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  spacing: 8.0,
+    return SafeArea(
+      child: Scaffold(
+        drawer: getDrawer(context),
+        appBar: getAppBar(context: context),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 32,
+              children: [
+                Center(child: Image.asset('assets/logo.png', width: 175.0)),
+                Text('Boas vindas!', style: TextStyle(fontSize: 16.0)),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'O que você deseja comer?',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                Text('Escolha por categoria', style: TextStyle(fontSize: 16.0)),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    spacing: 8.0,
+                    children: List.generate(
+                      CategoriesData.listCategories.length,
+                      (index) {
+                        return CategoryWidget(
+                          category: CategoriesData.listCategories[index],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Image.asset('assets/banners/banner_promo.png'),
+                Text('Bem avaliados', style: TextStyle(fontSize: 16.0)),
+                Column(
+                  spacing: 16.0,
                   children: List.generate(
-                    CategoriesData.listCategories.length,
+                    restaurantData.listRestaurants.length,
                     (index) {
-                      return CategoryWidget(
-                        category: CategoriesData.listCategories[index],
-                      );
+                      Restaurant restaurant =
+                          restaurantData.listRestaurants[index];
+                      return RestaurantWidget(restaurant: restaurant);
                     },
                   ),
                 ),
-              ),
-              Image.asset('assets/banners/banner_promo.png'),
-              Text('Bem avaliados'),
-              Column(
-                spacing: 16.0,
-                children: List.generate(restaurantData.listRestaurants.length, (
-                  index,
-                ) {
-                  Restaurant restaurant = restaurantData.listRestaurants[index];
-                  return RestaurantWidget(restaurant: restaurant);
-                }),
-              ),
-              SizedBox(height: 64.0),
-            ],
+                SizedBox(height: 64.0),
+              ],
+            ),
           ),
         ),
       ),
