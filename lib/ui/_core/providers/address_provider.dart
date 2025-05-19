75edb1c401b6b1147c8f2b257cf3928d4fbcbcde
Lucas David <lucasdavidoliveira80@gmail.com>
2025-05-19 20:28:40 -0300

    feat(address_provider): add calculo de frete

--- lib/ui/_core/providers/address_provider.dart
+++ lib/ui/_core/providers/address_provider.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class AddressProvider extends ChangeNotifier {
   String? _selectedAddressDelivery;
   String? _selectedAddressDetails;
+  double _restaurantDistance = 0;
 
   String? get selectedAddressDelivery => _selectedAddressDelivery;
   String? get selectedAddressDetails => _selectedAddressDetails;
+  double get restaurantDistance => _restaurantDistance;
 
   void selectAddressDelivery(String method, String details) {
     _selectedAddressDelivery = method;
@@ -21,11 +23,16 @@ class AddressProvider extends ChangeNotifier {
 
   double getDeliveryFee() {
     if (_selectedAddressDelivery == 'Casa') {
-      return 7.00;
+      return _restaurantDistance * .80;
     } else if (_selectedAddressDelivery == 'Trabalho') {
-      return 5.00;
+      return _restaurantDistance * .50;
     } else {
       return 0.00;
     }
+  }
+
+  void setRestaurantDistance(int distance) {
+    _restaurantDistance = _restaurantDistance + distance.toDouble();
+    notifyListeners();
   }
 }
