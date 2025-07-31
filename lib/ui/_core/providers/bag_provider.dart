05ed391a5852136b3f2807855819eb475a94f8a4
Lucas David <lucasdavidoliveira80@gmail.com>
2025-07-31 11:32:30 -0300

    feat(bag_provider): adicionando verificação do prato antes de add

--- lib/ui/_core/providers/bag_provider.dart
+++ lib/ui/_core/providers/bag_provider.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:techtaste/model/dish.dart';
+import 'package:techtaste/model/restaurant.dart';
 
 class BagProvider extends ChangeNotifier {
+  List<String> getRestaurantNames(List<Restaurant> restaurants) {
+    return restaurants.map((r) => r.name).toList();
+  }
+
   List<Dish> dishesOnBag = [];
 
   addAllDishes(List<Dish> dishes) {
@@ -39,5 +44,17 @@ class BagProvider extends ChangeNotifier {
       total += dish.price;
     }
     return total;
+  }
+
+  bool tryAddDish(Dish dish, String restaurantName) {
+    // Verifica se o prato já está no carrinho ou se o restaurante é o mesmo
+    if (dishesOnBag
+        .isEmpty /*||
+        dishesOnBag.any((d) => d.getRestaurantNames == restaurantName)*/ ) {
+      addAllDishes([dish]);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
